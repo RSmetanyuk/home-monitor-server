e2d4f108cec24383a9b1533a606cbb707cef54f1
Yevhen Motrych <i10111pn@gmail.com>
2019-07-25 07:18:53 +0300

    Added CORS

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.get('/temperature', (req, res) => {
-  res.send({
+  res.send([{
     'time': Date.now(),
-    'temperature': 23
-  });
+    'value': 29
+  }]);
 });
 
 app.listen(port);
