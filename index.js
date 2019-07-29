f701f48570f2568dd2bf57d43de7f1ab37998afa
Yevhen Motrych <i10111pn@gmail.com>
2019-07-29 04:54:56 +0300

    Added sockets

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const app = require('express')();
+const http = require('http');
+const socketIO = require('socket.io');
 
-const app = express();
-const port = process.env.PORT || 3000;
+const {
+  getRandomInt
+} = require('./helpers');
 
-app.use(cors());
+const server = http.createServer(app);
+const io = socketIO(server)
+const port = process.env.PORT || 5000;
 
-app.get('/temperature', (req, res) => {
-  res.send([{
-    'time': Date.now(),
-    'value': 29
-  }]);
+io.on('connection', socket => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  setInterval(
+    () => io.sockets.emit('temperature change', {
+      'time': Date.now(),
+      'value': getRandomInt(15, 29)
+    }),
+    getRandomInt(10000, 100000) //send new temperature every 10s-100s
+  );
 });
 
-app.listen(port);
+server.listen(port, () => console.log(`Listening on port ${port}`));
