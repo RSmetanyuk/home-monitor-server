4f0db9289f6e8021273760f1aa9ec1e532ba8551
Yevhen Motrych <i10111pn@gmail.com>
2019-08-01 04:53:46 +0300

    Changed socket update time and value range

--- helpers.js
+++ helpers.js
@@ -1,6 +1,13 @@
-exports.getRandomInt = function (min, max) {
+function _getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
+
+function _getRandomTemperature() {
+  return _getRandomInt(21, 23) + parseFloat(Math.random().toFixed(2));
+}
+
+exports.getRandomInt = _getRandomInt;
+exports.getRandomTemperature = _getRandomTemperature;
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const http = require('http');
 const socketIO = require('socket.io');
 
 const {
-  getRandomInt
+  getRandomInt,
+  getRandomTemperature
 } = require('./helpers');
 
 const server = http.createServer(app);
@@ -15,17 +16,23 @@ app.get('/', (req, res) => res.send(`The server is listening on port ${port}`));
 io.on('connection', socket => {
   console.log('New client connected');
 
+  function _emitTemperatureChange() {
+    io.sockets.emit('temperature change', {
+      'time': Date.now(),
+      'value': getRandomTemperature()
+    });
+
+    setTimeout(
+      _emitTemperatureChange,
+      getRandomInt(10000, 15000) //send new temperature every 10s-15s
+    );
+  }
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 
-  setInterval(
-    () => io.sockets.emit('temperature change', {
-      'time': Date.now(),
-      'value': getRandomInt(15, 29)
-    }),
-    getRandomInt(10000, 100000) //send new temperature every 10s-100s
-  );
+  _emitTemperatureChange();
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
